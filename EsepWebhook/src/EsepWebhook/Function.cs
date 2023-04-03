6866e399e5c1b583d53134af161e3634a0456d46
shivamurali <pandiham@gmail.com>
2023-04-02 20:52:30 -0400

    Second Push

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -1,4 +1,6 @@
 using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using System.Text;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
